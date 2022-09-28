d35d9946b0ada480af16ce08652d312acfc88394
Kapitar <kkapitar@ya.ru>
2022-09-28 22:13:07 +0300

    init

---
+++ geometry.cpp
@@ -0,0 +1,175 @@
+﻿/*
+VK: @kapitar
+руки прочь от моего решения
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣶⣶⣶⣶⣶⣶⣦⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⡟⠉⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣷⣦⣄⢈⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⡿⠛⢻⣿⣿⣿⣷⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠟⢉⣶⡞⠛⠉⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡾⢳⣶⡟⠉⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠘⠛⠀⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡋⠁⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣻⣿⣿⣿⣿⣿⡟⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣿⡟⠛⠛⠻⣿⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⢫⣿⣿⣿⣿⣿⡟⠀⠀⠀⢿⠟⣿⣿⣿⠛⠛⠋⠉⢁⣤⣶⠟⢻⣿⣿⣿⣿⡿⠃⠸⣿⣿⣧⡀⠀⢀⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⢁⣼⣿⣿⣿⣿⣿⣧⠀⣀⠀⠀⠀⣿⣿⢸⣿⣿⣿⣷⣿⣭⣤⣴⣿⣿⡿⠋⠉⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⢁⣾⣿⣿⣿⣿⣿⣿⣿⠠⠿⠀⠀⠀⣿⡏⠀⠙⣿⣿⣿⡏⣿⡿⠟⠛⠉⠀⠀⠀⠀⠐⠒⣿⣿⣿⣿⣿⣿⡷⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀⠀⠀⣿⡇⠀⠀⠉⠙⠋⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⠁⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⢁⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⢻⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣲⣿⠶⣦⣀⣿⣿⢿⣿⣿⣿⡇⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⣤⡀⠀⠀⠀⠙⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠧⢄⣿⣿⣿⡟⣼⣿⣿⣀⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠙⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⣨⡏⢹⣿⣧⣿⣿⠏⣿⠇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⢻⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡟⢡⣾⣿⣿⣿⡿⠐⠋⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠛⣦⡀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⣰⣿⢫⣴⣾⣿⡿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠙⢿⣆⠀⠀⠀⠈⠛⠛⠀⠀⠀⢀⣠⣿⠯⠈⠉⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⡟⠛⠙⠛⠿⣿⣯⣤⣤⣼⠿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣀⣤⣼⣿⣷⣄⠀⠀⣀⣀⣤⣶⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣾⡟⠉⠀⠀⠀⠀⠀⠀⠀⢠⣄⠀⠀⠀⢸⣿⣿⣿⣷⡄⠀⠀⡔⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢻⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡄⠀⠀⢸⣿⣿⣿⣿⡇⠀⠀⣿⡈⢿⣿⣿⣿⣿⣿⣿⡏⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠀⠀⠘⢿⣿⣿⣿⣇⠀⠀⠘⠋⠀⠙⢿⣿⣿⣿⣿⡇⠀⠘⣿⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠸⣷⡀⠀⢸⣿⣿⡿⣿⡀⠀⠀⠀⠀⠀⠀⡄⠙⠻⢿⣇⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⣠⣾⠇⠀⠀⠀⠀⢹⣧⡀⠘⣿⣿⡇⣿⣇⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠈⣷⣄⠈⣿⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⢀⣠⣾⠟⠁⠀⠀⠀⠀⠀⠀⢘⠿⠶⣿⣿⣿⡄⣿⠀⣀⡀⠀⠀⠀⠀⠀⠀⢠⡀⠀⠈⠀⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣸⠋⠀⣠⣴⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⢠⣾⠀⠀⢸⣿⣿⡇⢻⣿⣿⣿⣷⣀⠀⠀⠀⠀⢸⣷⡄⠀⠀⠈⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣿⣶⣾⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠈⢿⣿⣷⠾⣿⣿⠁⠹⣿⣧⡀⠀⠀⠀⢿⣿⡀⠀⠀⠙⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢠⣿⢿⣿⣿⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡇⠀⠀⠀⢸⣿⣿⢠⣿⣿⣷⠀⠘⢿⡇⠀⠀⠀⠀⠙⣷⣄⠀⠀⢻⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣿⠋⢾⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠀⠀⠀⢸⣿⣿⠀⠙⡿⣿⡀⠀⠀⢿⣇⠀⠀⠀⠀⠸⣿⡆⠀⠈⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣠⣟⣀⣄⣤⣶⣦⣀⣀⣀⢀⣴⣦⣀⡀⣠⣶⣶⣾⣿⡆⠀⠀⠀⢸⣿⣿⠀⠀⠁⠻⡇⠀⠀⠈⢻⡷⠀⠀⣀⣀⣹⣷⣄⠀⢸⣿⡀⠀⠀⠀⠀
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ld long double
+#define all(a) a.begin(), a.end()
+#define fori(i, j, n) for (int i = j; i < n; i++)
+
+typedef long long ll;
+typedef string str;
+typedef pair<int, int> pi;
+
+const int INF = 1e9;
+const int PI = static_cast<const int>(3.1415926535897932384626433832795);
+const int MOD = 1000000007;
+
+ld eps = 1e-6;
+
+struct Point {
+    ld x = 0, y = 0;
+
+    Point() {}
+    Point(ld x, ld y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    friend istream& operator >> (istream& stream, Point& P);
+    friend ostream& operator << (ostream& stream, const Point& P);
+
+    ld size() {
+        return sqrt(x * x + y * y);
+    }
+
+    Point perpendicular() {
+        return { -y, x };
+    }
+
+    void new_size(ld d) {
+        ld sz = size();
+        x = x / sz * d;
+        y = y / sz * d;
+    }
+};
+
+istream& operator >> (istream& stream, Point& P) {
+    stream >> P.x;
+    stream >> P.y;
+
+    return stream;
+}
+
+ostream& operator << (ostream& stream, const Point& P) {
+    stream << P.x << " ";
+    stream << P.y;
+
+    return stream;
+}
+
+struct Line {
+    ld a = 0, b = 0, c = 0;
+
+    Line() {}
+    Line(Point A, Point B) {
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = B.x * A.y - A.x * B.y;
+    }
+    Line(ld a, ld b, ld c) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+    Line(Point A, Line other) {
+        Point B = { A.x + other.a, A.y + other.b };
+        Line local = { A, B };
+
+        a = local.a;
+        b = local.b;
+        c = local.c;
+    }
+
+    friend ostream& operator << (ostream& stream, const Line& l);
+
+    ld dist(Point P) {
+        return abs(a * P.x + b * P.y + c) / sqrt(a * a + b * b);
+    }
+
+    bool isOnLine(Point P) {
+        return dist(P) < eps;
+    }
+
+    Point getPoint() {
+        if (a == 0) {
+            return { 0, -c / b };
+        }
+
+        return { -c / a, 0 };
+    }
+
+    bool isOnSameSide(Point A, Point B) {
+        return (a * A.x + b * A.y + c) * (a * B.x + b * B.y + c) >= 0;
+    }
+};
+
+ostream& operator << (ostream& stream, const Line& l) {
+    stream << l.a << " " << l.b << " " << l.c;
+
+    return stream;
+}
+
+void solve() {
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout.precision(16);
+
+        /*
+        #ifndef ONLINE_JUDGE
+            freopen("a.in", "r", stdin);
+            freopen("a.out", "w", stdout);
+        #endif
+        */
+
+    int t;
+    //cin >> t;
+    t = 1;
+
+    fori(tt, 0, t) {
+        //        cout << "Test #" << tt << ":\n";
+        solve();
+    }
+
+    return 0;
+}
